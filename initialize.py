383243f77c009947f8ec27e42500ae7920f69411
Quentin Chateiller <31767137+qcha41@users.noreply.github.com>
2019-10-14 20:24:26 +0200

    up

--- initialize.py
+++ initialize.py
@@ -4,17 +4,38 @@ Created on Mon Oct 14 19:59:58 2019
 
 @author: qchat
 """
+
+print()
+print()
+print('================')
+print(' COFFEE MANAGER ')
+print('================')
+print()
+print()
+print('Application starting, please wait...')
+print('')
+
+
 import os
 import time
 
+
+# Waiting for incoming connection
+print('Waiting for an internet connection, please wait...',end="\r")
 tini = time.time()
-maxDelay = 40
-
-print('coucou')
-print('coucou')
-
-time.sleep(10)
-        
+maxDelay = 40 #s
+result = False
+i = 0
+while time.time()-tini < maxDelay :
+    if i<100:#os.system('ping -c1 google.com') == 0 :
+        result = True
+        break
+    else :
+        i+=1
+        time.sleep(0.1)
+        print(f'Waiting for an internet connection, please wait... (%i)'%round(maxDelay-(time.time()-tini)),end="\r")
+print('Connected to internet!')
+print()
 
 def isConnected(self):
     return os.system('ping -c1 google.com') == 0

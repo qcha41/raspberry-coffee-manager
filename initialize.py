f2fda655a826085844bc78aa996cd451f5fcf82a
Quentin Chateiller <31767137+qcha41@users.noreply.github.com>
2019-10-14 21:03:02 +0200

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -21,29 +21,32 @@ import git
 # Waiting for incoming connection
 print('Waiting for an internet connection, please wait...',end="\r")
 tini = time.time()
-maxDelay = 40 #s
+maxDelay = 60 #s
 result = False
-i = 0
 while time.time()-tini < maxDelay :
     if os.system('ping -c1 google.com') == 0 :
         result = True
         break
     else :
-        i+=1
-        time.sleep(0.1)
         print('Waiting for an internet connection, please wait... (%i)'%round(maxDelay-(time.time()-tini)),end="\r")
-print()
-print('Connected to internet!')
-print()
-
-
-# Update git directory
-print('Updating git repository...')
-ans = git.Repo().git.pull()
-print(ans)
-print('Update finished!')
-print()
-
-# Starting GUI
-print('Starting GUI...')
-exec(open("./interface.py").read())
+        
+        
+if result is True :
+    print()
+    print('Connected to internet!')
+    print()
+    
+    # Update git directory
+    print('Updating git repository...')
+    ans = git.Repo().git.pull()
+    print(ans)
+    print('Update finished!')
+    print()
+    
+    # Starting GUI
+    print('Starting GUI...')
+    exec(open("./interface.py").read())
+    
+else :
+    # Restart pi
+    os.system('sudo shutdown -r now')

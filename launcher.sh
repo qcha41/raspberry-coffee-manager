a10c55460bb58d2117fbe1135fe30298312fffff
Quentin Chateiller <31767137+qcha41@users.noreply.github.com>
2019-10-14 21:49:17 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DISPLAY=:0 lxterminal -hold
-date
+DISPLAY=:0 lxterminal -hold date
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 sudo python3 initialize.py

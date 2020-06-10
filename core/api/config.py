59fb51ff17f35a48bbf7028371c21774b0d6f0b3
qcha41 <q.chateiller@gmail.com>
2020-06-11 00:55:29 +0200

    Update config.py

--- core/api/config.py
+++ core/api/config.py
@@ -15,6 +15,8 @@ def load_config():
     current_script_path = os.path.realpath(os.path.dirname(__file__))
     config_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(current_script_path))),'data','config.ini')
     
+    print(os.get_cwd())
+    
     if os.path.exists(config_file_path) is True : 
         config = configparser.ConfigParser()
         config.read(config_file_path)

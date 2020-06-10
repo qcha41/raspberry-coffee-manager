6a8bf93bd409580cc8f04f2792e5edcab6117eac
qcha41 <q.chateiller@gmail.com>
2020-06-10 22:30:55 +0200

    up

--- core/api/database/system.py
+++ core/api/database/system.py
@@ -9,7 +9,7 @@ from .. import config
 from .main import database
 from .users import User
 from . import utilities
-
+from operator import itemgetter
 
 
 # Users
@@ -359,9 +359,11 @@ def get_warnings() :
     for key in user_dict : 
         user = User(key)
         balance = user.get_balance()
-        if balance < 0 : balance_list.append(f'{user_dict[key]}: {balance} \u20ac')
+        if balance < 0 : balance_list.append([user_dict[key],balance])
         shares = user.get_total_shares()
         if shares == 0 : shares_list.append(f'{user_dict[key]}: No shares')
-        
+    
+    balance_list = [f'{entry[0]}: {entry[1]} \u20ac' for entry in sorted(balance_list, key=itemgetter(1))]
+    
     return balance_list + shares_list
     

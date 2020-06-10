d5e47b0e09ab64cbe0ae46e5e9e99f9ae2e7d054
Quentin <q.chateiller@gmail.com>
2020-06-10 08:42:20 +0200

    up

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-echo realpath "$0"
+SCRIPT=`realpath $0`
+SCRIPTFOLDER=`dirname $SCRIPT`
+MAINFOLDER=`dirname $SCRIPTFOLDER`
+
+mkdir -p data
+
+cp -n $SCRIPTFOLDER/data_template/config.ini $MAINFOLDER/data
+cp -n $SCRIPTFOLDER/data_template/database.db $MAINFOLDER/data
+
+echo "Installation complete! Now edit the config.ini file with your configuration."
+
